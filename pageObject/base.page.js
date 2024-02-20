53a397fbb361a261a1424a160a5959fba5207924
Priyanka-Bandaru <116892003+Priyanka-Bandaru@users.noreply.github.com>
2024-02-20 15:44:01 +0530

    failure test case added

--- pageObject/base.page.js
+++ pageObject/base.page.js
@@ -1,78 +1,157 @@
 require("dotenv").config();
 const actions = require("../utils/actions");
-exports.BasePage=class BasePage {
+exports.BasePage = class BasePage {
   constructor(page, test) {
     this.page = page;
     this.test = test;
-    this.landingPageLogo=page.locator("(//a[@aria-label='Target home'])[1]");
-    this.homePageSignBtn=page.locator("(//span[text()='Sign in'])[1]");
-    this.homePageSecondSignIn=page.locator("(//span[text()='Sign in'])[2]");
-    this.emailTab=page.locator("[id='username']");
-    this.passwordTab=page.locator("[name='password']");
-    this.signinBtn=page.locator("//button[@id='login']");
-    this.welcomeMessage=page.locator("(//span[contains(text(),'Hi')])[1]");
-    this.homePageSearchArea=page.locator("//input[@id='search']");
-    this.itemSortButton=page.locator("//div[text()='Sort']");
-    this.bestSellerItemButton=page.locator("//input[@id='bestselling-best seller']");
-    this.sortApplyButton=page.locator("//button[text()='Apply']");
-    this.resultItem=page.locator("(//section/div/div[2]//a)[2]");
-    this.productText=page.locator("//h1[@id='pdp-product-title-id']");
-    this.productAddToCartButton=page.locator("(//button[contains(@id,'addToCartButtonOrTextIdFor')])[1]");
-    this.shippingText=page.locator("//h3[text()='Shipping']");
-    this.closeCrossBtn=page.locator("//button[@aria-label='close']");
-    this.cartButton=page.locator("//a[contains(@aria-label,'cart')]");
-    this.itemAtCart=(itemName)=>page.locator("//div[contains(text(),'"+itemName+"')]");
-    this.removeItem=(itemName)=>page.locator("(//button[contains(@aria-label,'"+itemName+"')])[3]")
-    this.goToHome=page.locator("//a[text()='Go to homepage']");
-    this.signoutBtn=page.locator("//span[text()='Sign Out']");
+    this.landingPageLogo = page.locator("(//a[@aria-label='Target home'])[1]");
+    this.homePageSignBtn = page.locator("(//span[text()='Sign in'])[1]");
+    this.homePageSecondSignIn = page.locator("(//span[text()='Sign in'])[2]");
+    this.emailTab = page.locator("[id='username']");
+    this.passwordTab = page.locator("[name='password']");
+    this.signinBtn = page.locator("//button[@id='login']");
+    this.welcomeMessage = page.locator("(//span[contains(text(),'Hi')])[1]");
+    this.homePageSearchArea = page.locator("//input[@id='search']");
+    this.itemSortButton = page.locator("//div[text()='Sort']");
+    this.bestSellerItemButton = page.locator(
+      "//input[@id='bestselling-best seller']"
+    );
+    this.sortApplyButton = page.locator("//button[text()='Apply']");
+    this.resultItem = page.locator("(//section/div/div[2]//a)[2]");
+    this.productText = page.locator("//h1[@id='pdp-product-title-id']");
+    this.productAddToCartButton = page.locator(
+      "(//button[contains(@id,'addToCartButtonOrTextIdFor')])[1]"
+    );
+    this.shippingText = page.locator("//h3[text()='Shipping']");
+    this.closeCrossBtn = page.locator("//button[@aria-label='close']");
+    this.cartButton = page.locator("//a[contains(@aria-label,'cart')]");
+    this.itemAtCart = (itemName) =>
+      page.locator("//div[contains(text(),'" + itemName + "')]");
+    this.removeItem = (itemName) =>
+      page.locator("(//button[contains(@aria-label,'" + itemName + "')])[3]");
+    this.goToHome = page.locator("//a[text()='Go to homepage']");
+    this.assertPassword= page.locator("(//span[text()='Please enter a valid password'])[2]");
   }
 
-async launchApplication(url){
+  async launchApplication(url) {
     await this.page.goto(url);
     await this.page.waitForTimeout(parseInt(process.env.small_wait));
-}
-async signInIntoApplication(userName,password){
-   await actions.executeStep(this.test,this.homePageSignBtn,"click","click on sign button in landing page");
-   await actions.executeStep(this.test,this.homePageSecondSignIn,"click","click on sign button in landing page model");
-   await actions.executeStep(this.test,this.emailTab,"fill","enter user name",userName);
-   await actions.executeStep(this.test,this.passwordTab,"fill","enter password",password);
-   await actions.executeStep(this.test,this.signinBtn,"click","click on sign in button");
-}
+  }
+  async signInIntoApplication(userName, password) {
+    await actions.executeStep(
+      this.test,
+      this.homePageSignBtn,
+      "click",
+      "click on sign button in landing page"
+    );
+    await actions.executeStep(
+      this.test,
+      this.homePageSecondSignIn,
+      "click",
+      "click on sign button in landing page model"
+    );
+    await actions.executeStep(
+      this.test,
+      this.emailTab,
+      "fill",
+      "enter user name",
+      userName
+    );
+    await actions.executeStep(
+      this.test,
+      this.passwordTab,
+      "fill",
+      "enter password",
+      password
+    );
+    await actions.executeStep(
+      this.test,
+      this.signinBtn,
+      "click",
+      "click on sign in button"
+    );
+  }
 
-async serachForItem(currentItem){
-    await actions.executeStep(this.test,this.homePageSearchArea,"fill","search for "+currentItem+"in sear box",currentItem);
+  async serachForItem(currentItem) {
+    await actions.executeStep(
+      this.test,
+      this.homePageSearchArea,
+      "fill",
+      "search for " + currentItem + "in sear box",
+      currentItem
+    );
     await this.page.keyboard.press("Enter");
-}
+  }
 
-async applifiltersToSearch(){
-    await actions.executeStep(this.test,this.itemSortButton,"click","click on short button for sort items");
-    await actions.executeStep(this.test,this.bestSellerItemButton,"click","click on best seller in sort items");
-    await actions.executeStep(this.test,this.sortApplyButton,"click","click in apply sort button")
-}
+  async applifiltersToSearch() {
+    await actions.executeStep(
+      this.test,
+      this.itemSortButton,
+      "click",
+      "click on short button for sort items"
+    );
+    await actions.executeStep(
+      this.test,
+      this.bestSellerItemButton,
+      "click",
+      "click on best seller in sort items"
+    );
+    await actions.executeStep(
+      this.test,
+      this.sortApplyButton,
+      "click",
+      "click in apply sort button"
+    );
+  }
 
-async selectItemToAddCart(){
+  async selectItemToAddCart() {
     await this.page.waitForTimeout(parseInt(process.env.small_wait));
-    let curretItemName=await this.resultItem.innerText();
-    await actions.executeStep(this.test,this.resultItem,"click","click on result item");
+    let curretItemName = await this.resultItem.innerText();
+    await actions.executeStep(
+      this.test,
+      this.resultItem,
+      "click",
+      "click on result item"
+    );
     return curretItemName;
-}
+  }
 
-async addItemToCart(){
-    await actions.executeStep(this.test,this.productAddToCartButton,"click","click on product addto cart button");
-}
+  async addItemToCart() {
+    await actions.executeStep(
+      this.test,
+      this.productAddToCartButton,
+      "click",
+      "click on product addto cart button"
+    );
+  }
 
-async verifyItemIsadded(){
-    await actions.executeStep(this.test,this.closeCrossBtn,"click","close the shipping model");
-    await actions.executeStep(this.test,this.cartButton,"click","open cart item");
-}
-
-async removeItemFromCart(itemName){
-    await this.page.waitForTimeout(parseInt(process.env.medium_wait));
-    await actions.executeStep(this.test,this.cartButton,"click","open cart item");
-    await actions.executeStep(this.test,this.removeItem(itemName),"click","remove cart item");
-}
-async signOutFromApplication(){
-    await actions.executeStep(this.test,this.welcomeMessage,"click","Now click on profile");
-    await actions.executeStep(this.test,this.signoutBtn,"click","To click on Signout button");
-}
+  async verifyItemIsadded() {
+    await actions.executeStep(
+      this.test,
+      this.closeCrossBtn,
+      "click",
+      "close the shipping model"
+    );
+    await actions.executeStep(
+      this.test,
+      this.cartButton,
+      "click",
+      "open cart item"
+    );
+  }
+  async removeItemFromCart(itemName) {
+    await this.page.waitForTimeout(parseInt(process.env.small_wait));
+    await actions.executeStep(
+      this.test,
+      this.cartButton,
+      "click",
+      "open cart item"
+    );
+    await actions.executeStep(
+      this.test,
+      this.removeItem(itemName),
+      "click",
+      "remove cart item"
+    );
+  }
 };
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,5 @@
 import { test, expect } from "@playwright/test";
 import { BasePage } from "../pageObject/base.page";
-import { allure } from "allure-playwright";
 require("dotenv").config();
 
 test.describe("POC_UI Automation", () => {
@@ -11,45 +10,30 @@ test.describe("POC_UI Automation", () => {
     page = await browser.newPage();
     basePageObj = new BasePage(page, test);
     await basePageObj.launchApplication(process.env.BASE_URL);
- 
-      await expect(basePageObj.landingPageLogo).toBeVisible();
-
-    // await basePageObj.signInIntoApplication(
-    //   process.env.user_name,
-    //   process.env.pass_word
-    // );
-    //   await expect(basePageObj.welcomeMessage).toBeVisible();
+    await expect(basePageObj.landingPageLogo).toBeVisible();
   });
 
   test("Adding Item to cart based on the preference", async () => {
     await basePageObj.serachForItem("watch");
     await basePageObj.applifiltersToSearch();
     currentItemName = await basePageObj.selectItemToAddCart();
- 
-      await expect(basePageObj.productText).toHaveText(currentItemName);
-
+    await expect(basePageObj.productText).toHaveText(currentItemName);
     await basePageObj.addItemToCart();
-
-      await expect(basePageObj.shippingText).toBeVisible();
-
+    await expect(basePageObj.shippingText).toBeVisible();
     await basePageObj.verifyItemIsadded();
-   
-      await expect(
-        basePageObj.itemAtCart(currentItemName.slice(0, 10))
-      ).toBeVisible();
-  
+    await expect(
+      basePageObj.itemAtCart(currentItemName.slice(0, 10))
+    ).toBeVisible();
   });
 
   test("To remove items from the cart ", async () => {
-   
-      //await expect(basePageObj.welcomeMessage).toBeVisible();
-
     await basePageObj.removeItemFromCart(currentItemName.slice(0, 10));
   });
-  // test("Sign out of the application", async () => {
-
-  //     await expect(basePageObj.welcomeMessage).toBeVisible();
-
-  //   await basePageObj.signOutFromApplication();
-  // });
+  test("Failed test case with Login Scenario",async()=>{
+     await basePageObj.signInIntoApplication(
+      process.env.user_name,
+      process.env.pass_word
+    );
+    await expect(basePageObj.assertPassword).not.toBeVisible({ timeout: 3000 });
+  })
 });
